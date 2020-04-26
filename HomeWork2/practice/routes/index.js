e32f422733ff0369ba617d25a78ff6e05cee851a
최정균 <wjdrbs966@naver.com>
2020-04-26 20:46:16 +0900

    nodeJS blog.js add

--- HomeWork2/practice/routes/index.js
+++ HomeWork2/practice/routes/index.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.use('/api', require('./api'))
+router.use('/api', require('./api'));
+router.use('/blog', require('./blog'));
 
 module.exports = router;
