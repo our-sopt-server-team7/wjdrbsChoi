6279bf4a9c58181ed8bdc2b2c6f289fab07039a1
최정균 <wjdrbs966@naver.com>
2020-05-28 17:52:57 +0900

    Level1 풀더 adD

--- HomeWork4/practice/models/user.js
+++ HomeWork4/practice/models/user.js
@@ -1,13 +1,12 @@
-const pool = require('../modules/pool')
+const pool = require('../modules/pool');
 const table = 'user';
 
 const user = {
-    signup : async (name, password, email, phone) => {
-        const fields = 'name, password, email, phone';
-        const questions = '?, ?, ?, ?';
-        const values = [name, password, email, phone];
+    signup: async (id, name, password, salt, email) => {
+        const fields = 'id, name, password, salt, email';
+        const questions = `?, ?, ?, ?, ?`;
+        const values = [id, name, password, salt, email];
         const query = `INSERT INTO ${table}(${fields}) VALUES(${questions})`;
-
         try {
             const result = await pool.queryParamArr(query, values);
             const insertId = result.insertId;
@@ -21,28 +20,22 @@ const user = {
             throw err;
         }
     },
-
-    checkUser : async (id) => {
+    checkUser: async (id) => {
         const query = `SELECT * FROM ${table} WHERE id="${id}"`;
         try {
             const result = await pool.queryParam(query);
-            // 쿼리 결과가 존재하지 않을 때
             if (result.length === 0) {
                 return false;
-            }
-            // 쿼리 결과가 존재할 때
-            return true;
+            } else return true;
         } catch (err) {
             if (err.errno == 1062) {
                 console.log('signup ERROR : ', err.errno, err.code);
                 return -1;
             }
-
             console.log('signup ERROR : ', err);
             throw err;
         }
     },
-
     getUserById : async (id) => {
         const query = `SELECT * FROM ${table} WHERE id=?`;
         try {
@@ -52,12 +45,10 @@ const user = {
                 console.log('signup ERROR : ', err.errno, err.code);
                 return -1;
             }
-
             console.log('signup ERROR : ', err);
             throw err;
         }
     }
-    
 }
 
 module.exports = user;
