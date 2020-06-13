4d215dd27eff601881297781da72bc5043e9978a
최정균 <wjdrbs966@naver.com>
2020-06-13 15:50:29 +0900

    gitignore

--- HomeWork3/practice/config/database.js
+++
@@ -1,11 +0,0 @@
-const mysql = require('promise-mysql');
-
-const dbConfig = {
-    host: 'localhost:3306',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'test',
-}
-
-module.exports = mysql.createPool(dbConfig);
